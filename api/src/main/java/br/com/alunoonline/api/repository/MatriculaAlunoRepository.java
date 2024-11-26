23e0ada084cdf3e0de56c368a359a91f625f1191
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-11-26 15:37:50 -0300

    feat: Adicionando emitir historico

--- api/src/main/java/br/com/alunoonline/api/repository/MatriculaAlunoRepository.java
+++ api/src/main/java/br/com/alunoonline/api/repository/MatriculaAlunoRepository.java
@@ -4,8 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.alunoonline.api.model.MatriculaAluno;
+import java.util.List;
+
 
 @Repository
 public interface MatriculaAlunoRepository extends JpaRepository<MatriculaAluno, Long> {
+
+	// Cria uma lista das matriculas ligadas pelo id do aluno
+	List<MatriculaAluno> findByAlunoId(Long alunoId);
 
 }
--- api/src/main/java/br/com/alunoonline/api/service/MatriculaAlunoService.java
+++ api/src/main/java/br/com/alunoonline/api/service/MatriculaAlunoService.java
@@ -1,11 +1,16 @@
 package br.com.alunoonline.api.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import br.com.alunoonline.api.dtos.AtualizarNotasRequest;
+import br.com.alunoonline.api.dtos.DisciplinasAlunoResponse;
+import br.com.alunoonline.api.dtos.HistoricoAlunoResponse;
 import br.com.alunoonline.api.enums.MatriculaAlunoStatusEnum;
 import br.com.alunoonline.api.model.MatriculaAluno;
 import br.com.alunoonline.api.repository.MatriculaAlunoRepository;
@@ -103,6 +108,71 @@ public class MatriculaAlunoService {
 			MatriculaAlunoStatusEnum.REPROVADO);
 
 		}
+
+	}
+
+	//* Método para emitir histórico escolar do aluno
+	public HistoricoAlunoResponse emitirHistorico(Long alunoId) {
+
+		List<MatriculaAluno> matriculasDoAluno = matriculaAlunoRepository.findByAlunoId(alunoId);
+
+		//* Se o aluno não tiver matricula cadastrada
+		if (matriculasDoAluno.isEmpty()) {
+
+			// Lança a exceção para o front o código HTTP 404(Not Found)
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Esse aluno não possui nenhuma matricula");
+
+		}
+
+		// Instancia o DTO HistoricoAluno para ser posteriormente preenchido
+		HistoricoAlunoResponse historicoAluno = new HistoricoAlunoResponse();
+
+		//? Pega as informações do aluno através do repository e salva na nova instancia
+			historicoAluno.setNomeAluno(matriculasDoAluno.get(0).getAluno().getNome());
+			historicoAluno.setEmailAluno(matriculasDoAluno.get(0).getAluno().getEmail());
+			historicoAluno.setCpfAluno(matriculasDoAluno.get(0).getAluno().getCpf());
+
+		// Cria uma lista de disciplinas limpa
+		List<DisciplinasAlunoResponse> disciplinasList = new ArrayList<>();
+
+		//* Abre um forearch para setar as informações do DTO DisciplinasAluno 
+		for (MatriculaAluno matriculaAluno : matriculasDoAluno) {
+
+			// Instancia o DTO DisciplinasAluno para ser posteriormente preenchido
+			DisciplinasAlunoResponse disciplinasAlunoResponse = new DisciplinasAlunoResponse();
+
+			//? Pega o nome da disciplina através do repository e salva na nova instancia
+			disciplinasAlunoResponse.setNomeDisciplina(matriculaAluno.getDisciplina().getNome());
+
+			//? Pega o nome do professor através do repository e salva na nova instancia
+			disciplinasAlunoResponse.setNomeProfessor(matriculaAluno.getDisciplina().getProfessor().getNome());
+
+			disciplinasAlunoResponse.setNota1(matriculaAluno.getNota1());
+			disciplinasAlunoResponse.setNota2(matriculaAluno.getNota2());
+
+			//* Calcular a media
+			if (matriculaAluno.getNota1() != null && matriculaAluno.getNota2() != null) {
+
+				disciplinasAlunoResponse.setMedia((matriculaAluno.getNota1() + matriculaAluno.getNota2()) / 2.0);
+
+			}
+
+			else { //* Se não tiver as 2 notas
+
+				disciplinasAlunoResponse.setMedia(null);
+
+			}
+
+			disciplinasAlunoResponse.setStatus(matriculaAluno.getStatus());
+
+			// Atualiza a lista apos os set no for  
+			disciplinasList.add(disciplinasAlunoResponse);
+
+		}
+
+		// Salva as informações da lista
+		historicoAluno.setDisciplinasAlunoResponses(disciplinasList);
+		return historicoAluno;
 
 	}
 
